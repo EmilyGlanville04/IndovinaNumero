bf5d339a358b4a10c0c82a28a8c99f095f8c2149
EmilyGlanville04 <s322158@studenti.polito.it>
2026-03-17 15:43:10 +0100

    lezione in aula, implementazione di "Indovina numero"

--- controller.py
+++ controller.py
@@ -1,7 +1,44 @@
 from view import View
 from model import Model
 import flet as ft
+
 class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+
+    def getNmax(self):
+        return self._model.Nmax
+    def getTmax(self):
+        return self._model.Tmax
+
+
+    def reset(self,e):
+        self._model.reset() #resetto lo stato del gioco lato modello
+        self._view._txtT.value=self._model.T
+        self._view._lvOut.controls.clear()
+        self._view._lvOut.controls.append(ft.Text("Inizia il gioco! Indovina il numero che sto pensando"))
+        self._view.update()
+
+    def play(self,e):
+        tentativoStr = self._view._txtInTentativo.value
+        try:
+            tentativo = int(tentativoStr)
+        except ValueError:
+            self._view._lvOut.controls.append(ft.Text("Errore devi inserire un valore numerico"))
+            self._view.update()
+            return
+        res = self._model.play(tentativo)
+        if res == 0:
+            self._view._lvOut.controls.append(ft.Text(f"Hai vinto! Il valore corretto era: {tentativo}", color = "green"))
+            self._view.update()
+            return
+        elif res ==2:
+            self._view._lvOut.controls.append(ft.Text(f"Hai perso! Il valore corretto era: {self._model.segreto}"))
+            self._view.update()
+        elif res ==-1:
+            self._view._lvOut.controls.append(ft.Text(f"Ritenta! Il segreto è più piccolo di:  {tentativo}"),                         )
+            self._view.update()
+        else:
+            self._view._lvOut.controls.append(ft.Text(f"Ritenta!Il segreto è più grande di:  {tentativo}"))
--- model.py
+++ model.py
@@ -2,4 +2,55 @@ import random
 
 class Model(object):
     def __init__(self):
-        pass
+        self._Nmax = 100
+        self._Tmax = 6
+        self._T = self._Tmax
+        self._segreto = None
+
+    #resettare il gioco
+    def reset(self):
+        # cambiamo il numero segreto
+        self._segreto= random.randint(0, self._Nmax)
+        self._T=self._Tmax #riprisitna il numerp di tentativi rimanenti
+        print(self._segreto)
+
+    def play(self,tentativo):
+        #questo metodo riceve come argomento un valore intero,
+        # #che sarà il tentativo del giocatore
+        # e lo confronta con il segreto
+        self._T -= 1
+        if tentativo == self._segreto:
+            return 0
+        if self._T ==0:
+            return 2
+        if tentativo > self._segreto:
+            return -1
+        else:
+            return 1
+
+    @property
+    def Nmax(self):
+        return self._Nmax
+    @property
+    def Tmax(self):
+        return self._Tmax
+    @property
+    def T(self):
+        return self._T
+    @property
+    def segreto(self):
+        return self._segreto
+
+if __name__ == "__main__":
+    m = Model()
+    m.reset()
+    print(m.play(10))
+    print(m.play(20))
+    print(m.play(30))
+    print(m.play(70))
+    print(m.play(80))
+    print(m.play(60))
+    print(m.play(50))
+
+
+
